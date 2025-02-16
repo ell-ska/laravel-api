bd10fa3d6376e79aa368fe96967a7d8496e93893
ell-ska <ella@skagerstrom.se>
2025-02-16 23:15:34 +0100

    log in function

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Exceptions\AuthException;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
@@ -24,5 +25,26 @@ class AuthController extends Controller
         return response()->json([
             'message' => 'user registered',
         ], 201);
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'username' => ['required', 'string'],
+            'password' => ['required', 'string'],
+        ]);
+
+        $user = User::where('username', $request->username)->firstOrFail();
+
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            throw new AuthException('invalid credentials', 400);
+        }
+
+        $token = $user->createToken('access-token');
+
+        return response()->json([
+            'message' => 'user authenticated',
+            'access-token' => $token->plainTextToken,
+        ], 200);
     }
 }

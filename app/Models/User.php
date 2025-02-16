7931b82fc54b1ce2e690104bbaf60e6a0e38a3a5
ell-ska <ella@skagerstrom.se>
2025-02-16 22:39:26 +0100

    register function

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,8 +12,7 @@ class User extends Authenticatable
     use HasApiTokens, HasFactory, Notifiable;
 
     protected $fillable = [
-        'name',
-        'email',
+        'username',
         'password',
     ];
 

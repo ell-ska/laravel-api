8222b00a1ea9eddafa210496da35490b7419edbc
ell-ska <ella@skagerstrom.se>
2025-02-16 23:12:05 +0100

    global error handling

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,41 +3,26 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
-use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules;
-use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
     public function register(Request $request)
     {
-        try {
-            $request->validate([
-                'username' => ['required', 'string', 'max:255', 'regex:/^[a-zA-Z0-9]+$/', 'min:3', 'unique:'.User::class],
-                'password' => ['required', Rules\Password::defaults()],
-            ]);
+        $request->validate([
+            'username' => ['required', 'string', 'max:255', 'regex:/^[a-zA-Z0-9]+$/', 'min:3', 'unique:'.User::class],
+            'password' => ['required', Rules\Password::defaults()],
+        ]);
 
-            User::create([
-                'username' => $request->username,
-                'password' => Hash::make($request->password),
-            ]);
+        User::create([
+            'username' => $request->username,
+            'password' => Hash::make($request->password),
+        ]);
 
-            return response()->json([
-                'message' => 'user registered',
-            ], 201);
-        } catch (ValidationException $e) {
-            return response()->json([
-                'message' => 'validation failed',
-                'errors' => $e->errors(),
-            ], 400);
-        } catch (Exception $e) {
-            dump($e);
-
-            return response()->json([
-                'message' => 'failed to register user',
-            ], 500);
-        }
+        return response()->json([
+            'message' => 'user registered',
+        ], 201);
     }
 }
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Exceptions\AuthException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Validation\ValidationException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,5 +17,22 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(function (AuthException $e) {
+            return response()->json([
+                'message' => $e->getMessage(),
+            ], $e->getCode());
+        });
+
+        $exceptions->render(function (ValidationException $e) {
+            return response()->json([
+                'message' => 'validation failed',
+                'errors' => $e->errors(),
+            ], 400);
+        });
+
+        $exceptions->render(function (Exception $e) {
+            return response()->json([
+                'message' => 'something went horribly wrong',
+            ], 500);
+        });
     })->create();
